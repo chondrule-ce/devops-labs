9644bd77d5d3b6e74958ebfc9f1a9a865e8b21b2
chondrule-ce <232209937+chondrule-ce@users.noreply.github.com>
2025-09-26 22:25:16 +1000

    Rename files

--- flask-ci-cd-demo/app.py
+++ flask-ci-cd-demo/app.py
@@ -3,11 +3,11 @@ from flask import Flask
 # Use __name__ as the import_name to let Flask know the location of the application
 app = Flask(__name__)
 
-# Use the @ decorator to tell Flask to call the 'CI_demo' func when access the url '/'
+# Use the @ decorator to tell Flask to call the 'CICD_demo' func when access the url '/'
 # '/' represents the root path
 @app.route("/")
-def CI_demo():
-    return "DevOps CI demo"
+def CICD_demo():
+    return "DevOps CI/CD demo"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
